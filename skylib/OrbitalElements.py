dff8510ab690c3b2352a87c3cdbf246d08c5eef0
Johannes Bauer <joe@johannes-bauer.com>
2017-08-15 22:05:16 +0200

    Implement parsing of orbital elements
    For now, no math yet -- just parsing the elements themselves.

--- skylib/OrbitalElements.py
+++ skylib/OrbitalElements.py
@@ -20,5 +20,71 @@
 #   Johannes Bauer <JohannesBauer@gmx.de>
 
 class OrbitalElements(object):
-	def __init__(self):
-		pass
+	def __init__(self, a, e, i, Omega, omega, T, M):
+		self.__a = a
+		self.__e = e
+		self.__i = i
+		self.__Omega = Omega
+		self.__omega = omega
+		self.__T = T
+		self.__M = M
+
+	@property
+	def a(self):
+		return self.__a
+
+	@property
+	def e(self):
+		return self.__e
+
+	@property
+	def i(self):
+		return self.__i
+
+	@property
+	def Omega(self):
+		return self.__Omega
+
+	@property
+	def omega(self):
+		return self.__omega
+
+	@property
+	def T(self):
+		return self.__T
+
+	@property
+	def M(self):
+		return self.__M
+
+	@classmethod
+	def from_data(cls, data):
+		if "a" not in data:
+			raise Exception("Orbital element \"a\" (semimajor axis, given in AU) missing.")
+		if "e" not in data:
+			raise Exception("Orbital element \"e\" (ellipse eccentricity) missing.")
+		if "i" not in data:
+			raise Exception("Orbital element \"i\" (inclination, given in TODO) missing.")
+		if "Omega" not in data:
+			raise Exception("Orbital element \"Omega\" (longitude of the ascending node) missing.")
+		if "omega" not in data:
+			raise Exception("Orbital element \"omega\" (argument of the periapsis) missing.")
+		if "T" not in data:
+			raise Exception("Orbital element \"T\" (epoch, given as Julian Day) missing.")
+		if "M" not in data:
+			raise Exception("Orbital element \"M\" (object mass, given in solar masses) missing.")
+		return cls(a = data["a"], e = data["e"], i = data["i"], Omega = data["Omega"], omega = data["omega"], T = data["T"], M = data["M"])
+
+	def __str__(self):
+		elements = [
+				("a", self.a),
+				("e", self.e),
+				("i", self.i),
+				("Omega", self.Omega),
+				("omega", self.omega),
+				("T", self.T),
+				("M", self.M),
+		]
+		elements = ", ".join("%s = %.2e" % (key, value) for (key, value) in elements)
+		return "OrbitalElements<%s>" % (elements)
+
